7a1c1deabc3dfd98eb9caf699789ec30c699c630
lmtbelmonte <lmtbelmonte@gmail.com>
2023-10-07 09:17:50 +0200

    Primera Carga del Repo

---
+++ cicd-pipeline/pipeline-start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+tkn pipeline start tekton-greeter-pipeline-hub \
+ --serviceaccount='tekton-deployer-sa' \
+ --param GIT_REPO='https://github.com/lmt-cbs/tekton-tutorial-greeter.git' \
+ --param GIT_REF='master' \
+ --param CONTEXT_DIR='quarkus' \
+ --param DESTINATION_IMAGE='quay.io/lmtbelmonte01/tekton-greeter-pipeline:latest' \
+ --param IMAGE_DOCKERFILE='quarkus/Dockerfile' \
+ --param IMAGE_CONTEXT_DIR='quarkus' \
+ --param SCRIPT='kubectl create deploy tekton-greeter --image=quay.io/lmtbelmonte01/tekton-greeter-pipeline:latest' \
+ --workspace name=app-source,claimName=app-source-pvc \
+ --workspace name=maven-settings,emptyDir="" \
+ --use-param-defaults \
+ --showlog
